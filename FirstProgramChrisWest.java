b6a1405d7ca9549a606acbd598daf6bdaedeb62d
ChrisDub <39013525+ChrisDub@users.noreply.github.com>
2018-08-31 21:25:50 -0600

    Add files via upload

---
+++ FirstProgramChrisWest.java
@@ -0,0 +1,14 @@
+
+package first.program.chris.west;
+
+public class FirstProgramChrisWest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("   j     A    V     V   A");
+        System.out.println("   j    A A    V   V   A A");
+        System.out.println("j  j   AAAAA    V V   AAAAA");
+        System.out.println(" jj   A     A    V   A     A");
+    }   
+}
+ 
